f26839e440cccb5cb03689c2735992c4170b5409
saadiy2309 <sadriddin2309@gmail.com>
2023-08-12 23:53:29 +0500

    Update 709. To Lower Case.php

--- string/709. To Lower Case.php
+++ string/709. To Lower Case.php
@@ -9,6 +9,7 @@ echo toLowerCase($s);
 /*
     Could use more complex way of solving this quesetion such as:
     generating alphabet as key value pair with uppercase and lowercase letters
+    by using range('A','Z') range('a','z')
     then iterating over them and modifying to lowercase and inserting into array
     and converting array to string at the end
 
