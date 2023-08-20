571e0254165e9b8ea08cee9e0ae10f1061ff3140
saadiy2309 <sadriddin2309@gmail.com>
2023-08-21 00:05:53 +0500

    1967. Number of Strings That Appear as Substrings in Word.php

--- string/1967. Number of Strings That Appear as Substrings in Word.php
+++ string/1967. Number of Strings That Appear as Substrings in Word.php
@@ -1,10 +1,7 @@
 <?php
 $patterns = ["a","abc","bc","d"]; 
 $word = "abc";
-// var_dump(str_contains('a', 'abc')); 
-// die;
-// Output: 3
-// str_contains(string $haystack, string $needle): bool
+
 function numOfStrings($patterns, $word) {
         $counter = 0;
         for ($i=0; $i < count($patterns); $i++) { 
